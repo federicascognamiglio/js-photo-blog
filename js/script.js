b2c169304ca847c75d62b0b16404297a5d2967c6
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-20 11:27:37 +0100

    feat: overlay interaction completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 // DATI
 const rowElem = document.querySelector(".row");
+const overlayElem = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-btn");
 let dataArray = [];
 
 // FUNZIONI
+// Funzione che stampa Post
 const printPost = () => {
     dataArray.forEach((curItem) => {
         rowElem.innerHTML += `
             <div class="col">
-                <div class="photo-polaroid">
+                <div class="photo-polaroid" data-post-id="${curItem.id}">
                     <div class="photo">
                         <img src="${curItem.url}">
                     </div>
@@ -17,9 +20,31 @@ const printPost = () => {
     })
 }
 
+// Funzione che mostra Overlay
+const showOverlay = () => {
+    overlayElem.classList.remove("d-none");
+    overlayElem.classList.add("d-flex");
+}
+
+const hideOverlay = () => {
+    overlayElem.classList.remove("d-flex");
+    overlayElem.classList.add("d-none");
+}
+
+// Funzione che crea Event Listener su post
+const addEvent = () => {
+    const posts = document.querySelectorAll(".photo-polaroid");
+    posts.forEach((curPost) => {
+        curPost.addEventListener("click", showOverlay);
+    });
+};
+
 // ESECUZIONE LOGICA
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6", {timeout: 6000}).then((resp) => {
     dataArray = resp.data;
     console.log(dataArray);
     printPost()
+    addEvent()
 })
+
+closeBtn.addEventListener("click", hideOverlay);
