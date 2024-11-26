d6d354a5dc45caf3398f276222dfc3e5a8180b71
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-26 20:00:17 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ const rowElem = document.querySelector(".row");
 const overlayElem = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close-btn");
 const overlayImg = document.querySelector(".overlay-img");
-let dataArray = [];
 
 // FUNZIONI
-// Funzione che stampa Post
-const printPost = () => {
-    dataArray.forEach((curItem) => {
+//Print Card
+/**
+ * Funzione che stampa Post
+ * @param {array} array //array di oggetti da cui prendere i valori da inserire nella card
+ */
+const printPost = (array) => {
+    array.forEach((curItem) => {
         rowElem.innerHTML += `
             <div class="col">
                 <div class="photo-polaroid" data-post-id="${curItem.id}">
@@ -21,34 +24,50 @@ const printPost = () => {
     })
 };
 
-// Funzione che mostra Overlay
-const showOverlay = (clickElem) => {
+//Overlay
+/**
+ * Funzione che mostra Overlay
+ * @param {node} clickElem // elemeto da cui ottenere Id
+ * @param {array} array // array di elementi con cui confrontare Id
+ * @param {node} overlay // elemento da mostrare
+ */
+const showOverlay = (clickElem, array, overlay) => {
     const clickedPostId = clickElem.dataset.postId;
-    const imgToShowElem = dataArray.find((curItem) => curItem.id == clickedPostId);
+    const imgToShowElem = array.find((curItem) => curItem.id == clickedPostId);
     overlayImg.innerHTML = `<img src="${imgToShowElem.url}">`
-    overlayElem.classList.remove("d-none");
-    overlayElem.classList.add("d-flex");
+    overlay.classList.remove("d-none");
+    overlay.classList.add("d-flex");
 };
 
-// Funzione che nasconde Overlay
-const hideOverlay = () => {
-    overlayElem.classList.remove("d-flex");
-    overlayElem.classList.add("d-none");
+/**
+ * Funzione che nasconde Overlay
+ * @param {node} overlay // elemento da nascondere
+ */
+const hideOverlay = (overlay) => {
+    overlay.classList.remove("d-flex");
+    overlay.classList.add("d-none");
 };
 
-// Funzione che crea Event Listener su post
-const addEvent = () => {
+//Event Listener
+/**
+ * Funzione che crea Event Listener su post
+ * @param {array} photos // array di eleemnti a cui aggiungere evento
+ */
+const addEvent = (photos) => {
     const posts = document.querySelectorAll(".photo-polaroid");
     posts.forEach((curPost) => {
-        curPost.addEventListener("click", () => showOverlay(curPost));
+        curPost.addEventListener("click", () => showOverlay(curPost, photos, overlayElem));
     });
 };
 
-// ESECUZIONE LOGICA
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6", { timeout: 6000 }).then((resp) => {
-    dataArray = resp.data;
-    printPost()
-    addEvent()
-})
 
-closeBtn.addEventListener("click", hideOverlay);
+// ESECUZIONE LOGICA
+// Prendo dati tramite chiamata axios, uso i dati ottenuti per stampare le card e aggiungere un evento su ognuna
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6", { timeout: 3000 }).then((resp) => {
+    const photos = resp.data;
+    printPost(photos)
+    addEvent(photos)
+});
+
+// Aggiungo evento di chiusura overlay al bottone
+closeBtn.addEventListener("click", () => hideOverlay(overlayElem));
